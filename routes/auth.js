bff581f5e75120688393ffcd6f509f05658e86b4
Curtis Timson <curt@live.co.uk>
2018-03-05 20:56:43 +0000

    Add signup callback

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,21 @@
 module.exports = function(app, passport){
 
-    app.post('/api/auth/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
+    app.post('/api/auth/signup', function(req, res, next) {
+        
+        passport.authenticate('local-signup', function(err, user, info){
+
+            if (err) { return next(err); }
+            if (!user) { return res.redirect('/'); }
+
+            // req / res held in closure
+            req.logIn(user, function(err) {
+                if (err) {
+                    return next(err);
+                }
+                return res.send(user);
+            });
+        })(req, res, next);
+
+    });
     
 };
