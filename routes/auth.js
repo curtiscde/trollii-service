af89a53c40c2dd380b91b81f700bfb509403c179
Curtis Timson <curt@live.co.uk>
2018-03-05 21:04:47 +0000

    Add login logic and routing

--- routes/auth.js
+++ routes/auth.js
@@ -17,5 +17,11 @@ module.exports = function(app, passport){
         })(req, res, next);
 
     });
+
+    app.post('/api/auth/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
     
 };
