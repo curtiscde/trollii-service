eb9c61bd424faf5802daefdacb79f4ef2dcc064e
Curtis Timson <curt@live.co.uk>
2018-03-06 12:55:22 +0000

    Add ability to create items in list

--- routes/item.js
+++ routes/item.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose'); 
 
 var Item = mongoose.model('Item', {
+    listid: String,
+    userid: String,
     name : String
 });
 
 module.exports = function(app){
 
-    app.get('/api/item', function(req, res) {
+    app.get('/api/item/:listid', function(req, res) {
         // use mongoose to get all lists in the database
 
         if (!req.isAuthenticated()){
@@ -15,14 +17,48 @@ module.exports = function(app){
         else{
 
             Item.find(function(err, items) {
-                // if there is an error retrieving, send the error. nothing after res.send(err) will execute
                 if (err)
                     res.send(err)
-    
-                res.json(items); // return all lists in JSON format
+                res.json(getItems(items, req.params.listid));
             });
-            
+
         }
     });
 
+    app.post('/api/item', function(req, res) {
+
+        if (!req.body.listid || !req.body.name){
+            res.status(500).send({ error: 'listid and name cannot be blank' });
+        }
+        else{
+
+            Item.create({
+                listid: req.body.listid,
+                name : req.body.name,
+                userid: req.user._id
+            }, function(err, items) {
+                if (err)
+                    res.send(err);
+
+                Item.find(function(err, items) {
+                    if (err)
+                        res.send(err)
+                    res.json(getItems(items, req.body.listid));
+                });
+            });
+
+        }
+
+    });
+
+}
+
+var userHasListAccess = function(){
+
+}
+
+var getItems = function(items, listid){
+    return items.filter(function(item){
+        return item.listid == listid;
+    });
 }
