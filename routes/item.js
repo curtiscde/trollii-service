04b347fca47adf9c6893c89d4efb3eee6a5085d8
Curtis Timson <curt@live.co.uk>
2018-03-06 11:02:10 +0000

    Ensure user is logged in when item GET

--- routes/item.js
+++ routes/item.js
@@ -8,13 +8,21 @@ module.exports = function(app){
 
     app.get('/api/item', function(req, res) {
         // use mongoose to get all lists in the database
-        Item.find(function(err, items) {
-            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-            if (err)
-                res.send(err)
 
-            res.json(items); // return all lists in JSON format
-        });
+        if (!req.isAuthenticated()){
+            res.status(500).send({ error: 'Not logged in'});
+        }
+        else{
+
+            Item.find(function(err, items) {
+                // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+                if (err)
+                    res.send(err)
+    
+                res.json(items); // return all lists in JSON format
+            });
+            
+        }
     });
 
 }
