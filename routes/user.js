eec6f5ba64cf4fe3af940788271afac48d8b324d
Curtis Timson <curt@live.co.uk>
2018-03-11 16:26:33 +0000

    Add express jwt and jwks-rsa dependencies

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,27 @@
+var expressJwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+var jwtCheck = expressJwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://curt.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'https://trollii.com/',
+    issuer: "https://curt.auth0.com/",
+    algorithms: ['RS256'],
+    getToken: (req) => {
+        return req.params.token;
+    }
+});
+
+
 module.exports = function(apiRoutes){
 
-    apiRoutes.get('/user/profile', function(req, res) {
+    apiRoutes.get('/user/profile/:token', jwtCheck, function(req, res) {
+
+        console.log(req.user);
 
         if (!req.isAuthenticated()){
             res.send({
