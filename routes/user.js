df7fccec77ff98a6ba2938c7d0157b113980af28
Curtis Timson <curt@live.co.uk>
2018-03-12 16:15:08 +0000

    Switch user routing to use authJwt

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,8 @@
-var expressJwt = require('express-jwt');
-var jwks = require('jwks-rsa');
-
-var jwtCheck = expressJwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: "https://curt.auth0.com/.well-known/jwks.json"
-    }),
-    audience: 'https://trollii.com/',
-    issuer: "https://curt.auth0.com/",
-    algorithms: ['RS256'],
-    getToken: function fromHeaderOrQuerystring (req) {
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-            return req.headers.authorization.split(' ')[1];
-        } else if (req.query && req.query.token) {
-          return req.query.token;
-        }
-        return null;
-    }
-});
-
+var authJwt = require('../auth/jwt.js');
 
 module.exports = function(apiRoutes){
 
-    apiRoutes.get('/user/profile', jwtCheck, function(req, res) {
+    apiRoutes.get('/user/profile', authJwt.jwtCheck, function(req, res) {
 
         console.log(req.user);
 
