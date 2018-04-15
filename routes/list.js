4cc167c6ba609bd9db08f8145468b991aa89ab9f
Curtis Timson <curt@live.co.uk>
2018-04-15 15:23:50 +0100

    Remove logging

--- routes/list.js
+++ routes/list.js
@@ -10,13 +10,9 @@ module.exports = function(apiRoutes){
 
     // get all lists
     apiRoutes.get('/list', authJwt.jwtCheck, function(req, res) {
-        console.log('get /list');
-        console.log('Passed jwtCheck');
         List.find(function(err, lists) {
             if (err)
                 res.send(err)
-
-            console.log('lists', lists);
 
             res.json(listHelper.getUserLists(lists, req.user.sub));
         });
