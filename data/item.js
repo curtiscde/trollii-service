421b4709843bfb395faa0596fe7ef5a41e90276a
Curtis Timson <curt@live.co.uk>
2018-04-25 18:34:28 +0100

    Add new items to item data

--- data/item.js
+++ data/item.js
@@ -1,12 +1,14 @@
 let items = [
     { name: 'Apples', emoji: '🍎' },
+    { name: 'Avocado', emoji: '🥑' },
     { name: 'Bananas', emoji: '🍌' },
     { name: 'Cat food', emoji: '🐱' },
     { name: 'Dog food', emoji: '🐶' },
     { name: 'Eggs', emoji: '🥚' },
     { name: 'Honey', emoji: '🍯' },
     { name: 'Pears', emoji: '🍐' },
-    { name: 'Rice', emoji: '🍚' }
+    { name: 'Rice', emoji: '🍚' },
+    { name: 'Toothpaste', emoji: '😁' }
 ];
 
 module.exports = {
