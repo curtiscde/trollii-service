020cef7a30c0e150c30e518c4d8066449068be76
Curtis Timson <curt@live.co.uk>
2018-04-27 17:04:37 +0100

    Add additional emojis

--- data/item.js
+++ data/item.js
@@ -6,19 +6,27 @@ let items = [
     { name: 'Bread', emoji: '🍞' },
     { name: 'Bananas', emoji: '🍌' },
     { name: 'Bread', emoji: '🍞' },
+    { name: 'Cake', emoji: '🍰' },
     { name: 'Cat food', emoji: '🐱' },
     { name: 'Cheese', emoji: '🧀' },
     { name: 'Chocolate', emoji: '🍫' },
     { name: 'Coffee', emoji: '☕️' },
+    { name: 'Cookies', emoji: '🍪' },
     { name: 'Dog food', emoji: '🐶' },
     { name: 'Eggs', emoji: '🥚' },
     { name: 'Honey', emoji: '🍯' },
     { name: 'Milk', emoji: '🥛' },
+    { name: 'Pasta', emoji: '🍝' },
     { name: 'Pears', emoji: '🍐' },
+    { name: 'Pineapple', emoji: '🍍' },
+    { name: 'Popcorn', emoji: '🍿' },
     { name: 'Rice', emoji: '🍚' },
     { name: 'Shower Gel', emoji: '🚿' },
+    { name: 'Strawberries', emoji: '🍓' },
     { name: 'Tea Bags', emoji: '☕️' },
-    { name: 'Toothpaste', emoji: '😁' }
+    { name: 'Toothpaste', emoji: '😁' },
+    { name: 'Watermelon', emoji: '🍉' },
+    { name: 'Wine', emoji: '🍷' }
 ];
 
 module.exports = {
